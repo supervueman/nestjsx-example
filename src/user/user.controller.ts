c4f8735e2550667177dcc1e1b26cbceb7f8adffa
supervueman <supervueman@gmail.com>
2020-01-19 21:30:16 +0400

    Edits

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, Req, Body, Post, Put, Delete, Param } from '@nestjs/common';
+import { Controller, Get, Query, Req, Body, Post, Put, Delete, Param, Logger } from '@nestjs/common';
 import { Crud, CrudController, CrudOptions, Override, ParsedRequest, CrudRequest } from '@nestjsx/crud';
 import { UserDTO } from './user.dto';
 import { UserService } from './user.service';
@@ -20,32 +20,9 @@ export class UserController implements CrudController<User> {
   }
 
   @Override('getManyBase')
-  getManyBase(@ParsedRequest() req: CrudRequest) {
+  getAll(@ParsedRequest() req: CrudRequest) {
+    // tslint:disable-next-line: no-console
+    console.log(req);
     return this.base.getManyBase(req);
   }
-
-  // @Get()
-  // async indAll(): Promise<UserDTO[]> {
-  //   return await this.service.findAll();
-  // }
-
-  // @Get(':id')
-  // async findOne(@Param('id') id: string): Promise<UserDTO> {
-  //   return await this.service.findOne(Number(id));
-  // }
-
-  // @Post()
-  // async create(@Body() data: UserDTO): Promise<UserDTO> {
-  //   return await this.service.create(data);
-  // }
-
-  // @Put(':id')
-  // async update(@Param('id') id: string, @Body() data: Partial<UserDTO>): Promise<UserDTO> {
-  //   return await this.service.update(Number(id), data);
-  // }
-
-  // @Delete(':id')
-  // async delete(@Param('id') id: string): Promise<UserDTO> {
-  //   return await this.service.delete(Number(id));
-  // }
 }
