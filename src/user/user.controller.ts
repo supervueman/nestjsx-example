f099eb4fbc578ac8e5e2d540b7e9bbccf2e149bf
supervueman <supervueman@gmail.com>
2020-01-19 20:14:23 +0400

    Test crud package

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,33 +1,40 @@
 import { Controller, Get, Query, Req, Body, Post, Put, Delete, Param } from '@nestjs/common';
+import { Crud, CrudController } from '@nestjsx/crud';
 import { UserDTO } from './user.dto';
 import { UserService } from './user.service';
+import { User } from './user.entity';
 
+@Crud({
+  model: {
+    type: User,
+  },
+})
 @Controller('api/users')
-export class UserController {
-  constructor(private readonly userService: UserService) { }
+export class UserController implements CrudController<User> {
+  constructor(public service: UserService) { }
 
-  @Get()
-  async indAll(): Promise<UserDTO[]> {
-    return await this.userService.findAll();
-  }
+  // @Get()
+  // async indAll(): Promise<UserDTO[]> {
+  //   return await this.service.findAll();
+  // }
 
-  @Get(':id')
-  async findOne(@Param('id') id: string): Promise<UserDTO> {
-    return await this.userService.findOne(Number(id));
-  }
+  // @Get(':id')
+  // async findOne(@Param('id') id: string): Promise<UserDTO> {
+  //   return await this.service.findOne(Number(id));
+  // }
 
-  @Post()
-  async create(@Body() data: UserDTO): Promise<UserDTO> {
-    return await this.userService.create(data);
-  }
+  // @Post()
+  // async create(@Body() data: UserDTO): Promise<UserDTO> {
+  //   return await this.service.create(data);
+  // }
 
-  @Put(':id')
-  async update(@Param('id') id: string, @Body() data: Partial<UserDTO>): Promise<UserDTO> {
-    return await this.userService.update(Number(id), data);
-  }
+  // @Put(':id')
+  // async update(@Param('id') id: string, @Body() data: Partial<UserDTO>): Promise<UserDTO> {
+  //   return await this.service.update(Number(id), data);
+  // }
 
-  @Delete(':id')
-  async delete(@Param('id') id: string): Promise<UserDTO> {
-    return await this.userService.delete(Number(id));
-  }
+  // @Delete(':id')
+  // async delete(@Param('id') id: string): Promise<UserDTO> {
+  //   return await this.service.delete(Number(id));
+  // }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,60 +1,62 @@
-import { Injectable, Logger, OnModuleInit, HttpStatus, HttpException } from '@nestjs/common';
+import { Injectable, Logger, HttpStatus, HttpException } from '@nestjs/common';
+import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { UserDTO } from './user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
-import { throws } from 'assert';
 
 @Injectable()
-export class UserService {
+export class UserService extends TypeOrmCrudService<User> {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
-  ) { }
-
-  async findAll(): Promise<UserDTO[]> {
-    const users = await this.userRepository.find();
-    Logger.log(users);
-    return users;
+  ) {
+    super(userRepository);
   }
 
-  async findOne(id: number): Promise<UserDTO> {
-    const user = await this.userRepository.findOne({ where: { id } });
+  // async findAll(): Promise<UserDTO[]> {
+  //   const users = await this.userRepository.find();
+  //   Logger.log(users);
+  //   return users;
+  // }
 
-    if (!user) {
-      throw new HttpException('Error: ', HttpStatus.NOT_FOUND);
-    }
+  // async findByPk(id: number): Promise<UserDTO> {
+  //   const user = await this.userRepository.findOne({ where: { id } });
 
-    return user;
-  }
+  //   if (!user) {
+  //     throw new HttpException('Error: ', HttpStatus.NOT_FOUND);
+  //   }
 
-  async create(data: UserDTO): Promise<UserDTO> {
-    let user = await this.userRepository.create(data);
-    user = await this.userRepository.save(user);
-    return user;
-  }
+  //   return user;
+  // }
 
-  async update(id: number, data: Partial<UserDTO>): Promise<UserDTO> {
-    const user = await this.userRepository.findOne({ where: { id } });
+  // async create(data: UserDTO): Promise<UserDTO> {
+  //   let user = await this.userRepository.create(data);
+  //   user = await this.userRepository.save(user);
+  //   return user;
+  // }
 
-    if (!user) {
-      throw new HttpException('Not found', HttpStatus.NOT_FOUND);
-    }
+  // async update(id: number, data: Partial<UserDTO>): Promise<UserDTO> {
+  //   const user = await this.userRepository.findOne({ where: { id } });
 
-    await this.userRepository.update({ id }, data);
+  //   if (!user) {
+  //     throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+  //   }
 
-    return await this.userRepository.findOne({ where: { id } });
-  }
+  //   await this.userRepository.update({ id }, data);
 
-  async delete(id: number): Promise<UserDTO> {
-    const user = await this.userRepository.findOne({ where: { id } });
+  //   return await this.userRepository.findOne({ where: { id } });
+  // }
 
-    if (!user) {
-      throw new HttpException('Not found', HttpStatus.NOT_FOUND);
-    }
+  // async delete(id: number): Promise<UserDTO> {
+  //   const user = await this.userRepository.findOne({ where: { id } });
 
-    await this.userRepository.delete(id);
-    Logger.log(id);
-    return await this.userRepository.findOne({ where: { id } });
-  }
+  //   if (!user) {
+  //     throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+  //   }
+
+  //   await this.userRepository.delete(id);
+  //   Logger.log(id);
+  //   return await this.userRepository.findOne({ where: { id } });
+  // }
 }
